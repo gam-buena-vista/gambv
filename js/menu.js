00b55fd0b0e4f407e822eb347f0485f2cfbccc3c
gam-buena-vista <gambv1710@gmail.com>
2025-01-27 01:11:18 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const header = document.createElement("header");
     header.innerHTML = `
-        <img src="./Imagenes/logogamnegro.png" class="logo" alt="Logo del Gobierno Autónomo Municipal de Buena Vista">
+        <img src="Imagenes/logogamnegro.png" class="logo" alt="Logo del Gobierno Autónomo Municipal de Buena Vista">
         <input type="checkbox" id="menu" aria-label="Abrir menú">
         <label for="menu">
-            <img class="menu-icono" src="./Imagenes/menu.png" alt="Ícono de menú">
+            <img class="menu-icono" src="Imagenes/menu.png" alt="Ícono de menú">
         </label>
         <nav class="navbar">
             <ul>
-                <li><a href="./index.html">Inicio</a></li>
+                <li><a href="index.html">Inicio</a></li>
                 <li>
                     <a href="#">Gobierno Municipal ▼</a>
                     <ul>
